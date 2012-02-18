0cd30fa3c80ec023f9d469551f7628d5364df0f2
Schlaefer <openmail+sourcecode@siezi.com>
2012-02-18 11:09:55 +0100

    migrated Helper test suite

--- app/tests/groups/helpers.group.php
+++
@@ -1,9 +0,0 @@
-<?php
-class HelpersGroupTest extends TestSuite {
-	public $label = 'All App Helpers';
-
-	function HelpersGroupTest() {
-		TestManager::addTestCasesFromDirectory($this, APP_TEST_CASES . DS . 'helpers');
-	}
-}
-?>
