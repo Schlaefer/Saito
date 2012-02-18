e6cfbf2d64c3cd768a8deba7dd6bf4b4458de7e5
Schlaefer <openmail+sourcecode@siezi.com>
2012-02-18 10:33:25 +0100

    migrated glasenuhr helper test

--- app/Test/Case/View/Helper/GlasenuhrHelperTest.php
+++ app/Test/Case/View/Helper/GlasenuhrHelperTest.php
@@ -1,20 +1,26 @@
 <?php
 
-App::import('Helper', 'Glasenuhr');
+	App::uses('Controller', 'Controller');
+	App::uses('View', 'View');
+	App::uses('GlasenuhrHelper', 'View/Helper');
 
 class GlasenuhrHelperTest extends CakeTestCase {
 
-	function startTest($message) {
-		echo "<h3>Starting ".get_class($this)." - $message()</h3>\n";
-		$this->Glasenuhr =& new GlasenuhrHelper();
+	public function setUp() {
+		parent::setUp();
+
+		$Controller = new Controller();
+		$View = new View($Controller);
+		$this->Glasenuhr = new GlasenuhrHelper($View);
 	}
 
-	function endTest() {
+	public function tearDown() {
+		parent::tearDown();
 		unset($this->Glasenuhr);
 		ClassRegistry::flush();
 	}
 
-	function testFtime() {
+	public function testFtime() {
 
 		$watches = array (
 				0 => __('Nacht'),
@@ -30,57 +36,57 @@ class GlasenuhrHelperTest extends CakeTestCase {
 		$input 		= mktime(0, 0);
 		$expected = '8 Gl. Abend';
 		$result 	=	$this->Glasenuhr->ftime($input);
-		$this->assertIdentical($expected, $result);
+		$this->assertSame($expected, $result);
 
 		$input 		= mktime(0, 29);
 		$expected = '8 Gl. Abend';
 		$result 	=	$this->Glasenuhr->ftime($input);
-		$this->assertIdentical($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
+		$this->assertSame($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
 
 		$input 		= mktime(0, 30, 0);
 		$expected = '1 Gl. Nacht';
 		$result 	=	$this->Glasenuhr->ftime($input);
-		$this->assertIdentical($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
+		$this->assertSame($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
 
 		$input 		= mktime(0, 31, 0);
 		$expected = '1 Gl. Nacht';
 		$result 	=	$this->Glasenuhr->ftime($input);
-		$this->assertIdentical($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
+		$this->assertSame($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
 
 		$input 		= mktime(4, 59, 0);
 		$expected = '1 Gl. Morgen';
 		$result 	=	$this->Glasenuhr->ftime($input);
-		$this->assertIdentical($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
+		$this->assertSame($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
 
 		$input 		= mktime(9, 0, 0);
 		$expected = '2 Gl. Vormittag';
 		$result 	=	$this->Glasenuhr->ftime($input);
-		$this->assertIdentical($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
+		$this->assertSame($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
 
 		$input 		= mktime(13, 31);
 		$expected = '3 Gl. Nachmittag';
 		$result 	=	$this->Glasenuhr->ftime($input);
-		$this->assertIdentical($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
+		$this->assertSame($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
 
 		$input 		= mktime(18, 06);
 		$expected = '4 Gl. Freiwache';
 		$result 	=	$this->Glasenuhr->ftime($input);
-		$this->assertIdentical($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
+		$this->assertSame($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
 
 		$input 		= mktime(22, 44);
 		$expected = '5 Gl. Abend';
 		$result 	=	$this->Glasenuhr->ftime($input);
-		$this->assertIdentical($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
+		$this->assertSame($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
 
 		$input 		= mktime(15, 17);
 		$expected = '6 Gl. Nachmittag';
 		$result 	=	$this->Glasenuhr->ftime($input);
-		$this->assertIdentical($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
+		$this->assertSame($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
 
 		$input 		= mktime(23, 50);
 		$expected = '7 Gl. Abend';
 		$result 	=	$this->Glasenuhr->ftime($input);
-		$this->assertIdentical($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
+		$this->assertSame($expected, $result, "%s (" . strftime('%H:%M', $input). ')');
 
 	}
 
