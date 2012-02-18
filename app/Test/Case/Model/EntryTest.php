cd38e21ce14cc7f9a3394f8f1105209354535f6a
Schlaefer <openmail+sourcecode@siezi.com>
2012-02-18 16:05:09 +0100

    migrated model entry test case

--- app/Test/Case/Model/EntryTest.php
+++ app/Test/Case/Model/EntryTest.php
@@ -1,77 +1,74 @@
 <?php
-/* Entry Test cases generated on: 2010-07-08 18:07:15 : 1278607395*/
-App::import('Model', 'Entry');
 
-class EntryTest extends CakeTestCase {
-	var $fixtures = array('app.user', 'app.user_online', 'app.entry', 'app.category', 'app.smiley', 'app.smiley_code', 'app.setting', 'app.upload');
+	App::uses('Entry', 'Model');
 
-	public function testBeforeValidate() {
+	class EntryTest extends CakeTestCase {
 
-		//* save entry with text
-		$entry['Entry'] = array(
-				'user_id' => 3,
-				'subject'	=> 'Test Subject',
-				'Text'		=> 'Text Text',
-				'pid'			=> '2',
+		public $fixtures = array( 'app.user', 'app.user_online', 'app.entry', 'app.category', 'app.smiley', 'app.smiley_code', 'app.setting', 'app.upload' );
+
+		public function testBeforeValidate() {
+
+			//* save entry with text
+			$entry['Entry'] = array(
+					'user_id' => 3,
+					'subject' => 'Test Subject',
+					'Text' => 'Text Text',
+					'pid' => '2',
 			);
-
-	}
-
-	public function testToggle() {
-
-		$this->Entry->id = 2;
-
-		//* test that thread is unlocked 
-		$result = $this->Entry->field('locked');
-		$this->assertFalse($result);
-
-		//* lock thread
-		$this->Entry->toggle('locked');
-		$result = $this->Entry->field('locked');
-		$this->assertTrue($result);
-
-		//* unlock thread again
-		$this->Entry->toggle('locked');
-		$result = $this->Entry->field('locked');
-		$this->assertFalse($result);
-	}
-
-	public function testDeleteTree() {
-
-		//* test thread exists before we delete it
-		$result = $this->Entry->find('count', array('conditions' => array('tid' => '1')));
-		$expected = 3;
-		$this->assertEqual($result, $expected);
-
-		//* try to delete subentry
-		$this->Entry->id = 2; 
-		$result = $this->Entry->deleteTree();
-		$this->assertFalse($result);
-
-		$result = $this->Entry->find('count', array('conditions' => array('tid' => '1')));
-		$expected = 3;
-		$this->assertEqual($result, $expected);
-
-		//* try to delete thread
-		$this->Entry->id = 1; 
-		$result = $this->Entry->deleteTree();
-		$this->assertTrue($result);
-
-		$result = $this->Entry->find('count', array('conditions' => array('tid' => '1')));
-		$expected = 0;
-		$this->assertEqual($result, $expected);
-
 		}
 
-	function startTest($message) {
-		$this->Entry =& ClassRegistry::init('Entry');
+		public function testToggle() {
+
+			$this->Entry->id = 2;
+
+			//* test that thread is unlocked
+			$result = $this->Entry->field('locked');
+			$this->assertTrue($result == FALSE);
+
+			//* lock thread
+			$this->Entry->toggle('locked');
+			$result = $this->Entry->field('locked');
+			$this->assertTrue($result == TRUE);
+
+			//* unlock thread again
+			$this->Entry->toggle('locked');
+			$result = $this->Entry->field('locked');
+			$this->assertTrue($result == FALSE);
+		}
+
+		public function testDeleteTree() {
+
+			//* test thread exists before we delete it
+			$result = $this->Entry->find('count',
+					array( 'conditions' => array( 'tid' => '1' ) ));
+			$expected = 3;
+			$this->assertEqual($result, $expected);
+
+			//* try to delete subentry
+			$this->Entry->id = 2;
+			$result = $this->Entry->deleteTree();
+			$this->assertFalse($result);
+
+			$result = $this->Entry->find('count',
+					array( 'conditions' => array( 'tid' => '1' ) ));
+			$expected = 3;
+			$this->assertEqual($result, $expected);
+
+			//* try to delete thread
+			$this->Entry->id = 1;
+			$result = $this->Entry->deleteTree();
+			$this->assertTrue($result);
+
+			$result = $this->Entry->find('count',
+					array( 'conditions' => array( 'tid' => '1' ) ));
+			$expected = 0;
+			$this->assertEqual($result, $expected);
+		}
+
+		public function startTest() {
+			$this->Entry = ClassRegistry::init('Entry');
+		}
+
 	}
 
-	function endTest() {
-		unset($this->Entry);
-		ClassRegistry::flush();
-	}
-
-
-}
 ?>
--- app/Test/Fixture/EntryFixture.php
+++ app/Test/Fixture/EntryFixture.php
@@ -1,55 +1,56 @@
 <?php
-class EntryFixture extends CakeTestFixture {
-	var $name = 'Entry';
-	var $import = array('table'=>'entries');
 
-	public $records = array (
-		//* thread 1
-		array(
-			'id' => 1,
-			'subject'	=> 'First_Subject',
-			'text'		=> 'First_Text',
-			'pid'			=> 0,
-			'tid'			=> 1,
-			'time'		=> '2000-01-01 20:00:00',
-			// accession = 0
-			'category'	=> 2,
-			'user_id'		=> 3,
-		),
-		array(
-			'id' => 2,
-			'subject'	=> 'Second_Subject',
-			'text'		=> 'Second_Text',
-			'pid'			=> 1,
-			'tid'			=> 1,
-			'time'		=> '2000-01-01 20:01:00',
-			'category'	=> 2,
-			'user_id'		=> 2,
-		),
-		array(
-			'id' => 3,
-			'subject'	=> 'Third_Subject',
-			'text'		=> 'Third_Text',
-			'pid'			=> 2,
-			'tid'			=> 1,
-			'time'		=> '2000-01-01 20:02:00',
-			'category'	=> 2,
-			'user_id'		=> 3,
-		),
-		//* thread 2
-		array(
-			'id' => 4,
-			'subject'	=> 'Second Thread First_Subject',
-			'text'		=> '',
-			'pid'			=> 0,
-			'tid'			=> 4,
-			'time'		=> '2000-01-01 10:00:00',
-			// accession = 1
-			'category'	=> 4,
-			'user_id'		=> 1,
+	class EntryFixture extends CakeTestFixture {
 
-		)
-	);
+		public $import = array( 'table' => 'entries' );
 
-}
+		public $records = array(
+				//* thread 1
+				array(
+						'id' => 1,
+						'subject' => 'First_Subject',
+						'text' => 'First_Text',
+						'pid' => 0,
+						'tid' => 1,
+						'time' => '2000-01-01 20:00:00',
+						// accession = 0
+						'category' => 2,
+						'user_id' => 3,
+				),
+				array(
+						'id' => 2,
+						'subject' => 'Second_Subject',
+						'text' => 'Second_Text',
+						'pid' => 1,
+						'tid' => 1,
+						'time' => '2000-01-01 20:01:00',
+						'category' => 2,
+						'user_id' => 2,
+				),
+				array(
+						'id' => 3,
+						'subject' => 'Third_Subject',
+						'text' => 'Third_Text',
+						'pid' => 2,
+						'tid' => 1,
+						'time' => '2000-01-01 20:02:00',
+						'category' => 2,
+						'user_id' => 3,
+				),
+				//* thread 2
+				array(
+						'id' => 4,
+						'subject' => 'Second Thread First_Subject',
+						'text' => '',
+						'pid' => 0,
+						'tid' => 4,
+						'time' => '2000-01-01 10:00:00',
+						// accession = 1
+						'category' => 4,
+						'user_id' => 1,
+				)
+		);
+
+	}
+
 ?>
