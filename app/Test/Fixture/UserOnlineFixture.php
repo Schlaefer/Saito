781d59310edb1abf0001270e78739d29174426aa
Schlaefer <openmail+sourcecode@siezi.com>
2012-02-18 15:18:51 +0100

    migrated user-online model tests

--- app/Test/Fixture/UserOnlineFixture.php
+++ app/Test/Fixture/UserOnlineFixture.php
@@ -1,24 +1,25 @@
 <?php
 
-class UserOnlineFixture extends CakeTestFixture {
-	var $name = 'UserOnline';
-	var $table	= 'useronline';
+	class UserOnlineFixture extends CakeTestFixture {
 
-	var $fields = array (
-		'id'			=> array( 'type' => 'integer',	'length' => 11, 'key' => 'primary' ),
-		'user_id' => array( 'type' => 'string', 'length' => 32, 'default' => NULL ),
-		'time' 		=> array( 'type' => 'integer', 'length' => 14, 'default' => 0 ),
-		'logged_in'	=> array( 'type' => 'integer'   , 'length' => 1 ),
-	);
+		public $table = 'useronline';
+		public $fields = array(
+				'id' => array( 'type' => 'integer', 'length' => 11, 'key' => 'primary' ),
+				'user_id' => array( 'type' => 'string', 'length' => 32, 'default' => NULL ),
+				'time' => array( 'type' => 'integer', 'length' => 14, 'default' => 0 ),
+				'logged_in' => array( 'type' => 'integer', 'length' => 1 ),
+		);
 
-	/*
-	var $records = array (
-			// 2010-06-17 09:05:28 GMT
-			array ( 'id' => '1', 'user_id' => '1', 'time' => 1276765527, 'ip' => 'uid_1' ),
-			array ( 'id' => '2', 'user_id' => '2', 'time' => 1276765528, 'ip' => 'uid_2' ),
-			array ( 'id' => '3', 'user_id' => '3', 'time' => 1276765528, 'ip' => 'uid_3' ),
-			array ( 'id' => '10', 'user_id' => '0', 'time' => 1276765528, 'ip' => '192.168.0.10' ),
-	);
-	*/
-}
+		/*
+		  var $records = array (
+		  // 2010-06-17 09:05:28 GMT
+		  array ( 'id' => '1', 'user_id' => '1', 'time' => 1276765527, 'ip' => 'uid_1' ),
+		  array ( 'id' => '2', 'user_id' => '2', 'time' => 1276765528, 'ip' => 'uid_2' ),
+		  array ( 'id' => '3', 'user_id' => '3', 'time' => 1276765528, 'ip' => 'uid_3' ),
+		  array ( 'id' => '10', 'user_id' => '0', 'time' => 1276765528, 'ip' => '192.168.0.10' ),
+		  );
+		 */
+
+	}
+
 ?>
