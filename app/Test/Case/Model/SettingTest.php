be8331273713a3710c3dc6a822b4c5feda577a90
Schlaefer <openmail+sourcecode@siezi.com>
2012-02-18 15:26:34 +0100

    migrated setting model tests

--- app/Test/Case/Model/SettingTest.php
+++ app/Test/Case/Model/SettingTest.php
@@ -1,76 +1,75 @@
 <?php
-/* Setting Test cases generated on: 2010-06-22 13:06:43 : 1277204743*/
-App::import('Model', 'Setting');
 
-class SettingTest extends CakeTestCase {
-	var $fixtures = array('app.setting');
+	App::uses('Setting', 'Model');
 
-	protected $settingsCompact = array(
-			'forum_name' => 'macnemo',
-			'autolink' =>	'1',
-			'userranks_ranks' => '10=Castaway|20=Other|30=Dharma|100=Jacob',
-			'smilies' => 1,
-			'topics_per_page'	=> 20,
+	class SettingTest extends CakeTestCase {
+
+		public $fixtures = array( 'app.setting' );
+
+		protected $settingsCompact = array(
+				'forum_name' => 'macnemo',
+				'autolink' => '1',
+				'userranks_ranks' => '10=Castaway|20=Other|30=Dharma|100=Jacob',
+				'smilies' => 1,
+				'topics_per_page' => 20,
 		);
 
-	public function testAfterSave() {
-			
-			$data = array(
-					'value'	=> 'test',
-				);
+		public function testAfterSave() {
 
-			$this->Setting->id	= 'forum_name';
+			$data = array(
+					'value' => 'test',
+			);
+
+			$this->Setting->id = 'forum_name';
 			$this->Setting->save($data);
 
 			$result = $this->Setting->getSettings();
-			$expected = array_merge($this->settingsCompact, array('forum_name'=>'test', 'autolink'=>1) );
+			$expected = array_merge($this->settingsCompact,
+					array( 'forum_name' => 'test', 'autolink' => 1 ));
 			$this->assertEqual($result, $expected);
 		}
 
-	public function testGetSettings() {
-		$result = $this->Setting->getSettings();
-		$expected	= $this->settingsCompact;
-		$this->assertEqual($result, $expected);
+		public function testGetSettings() {
+			$result = $this->Setting->getSettings();
+			$expected = $this->settingsCompact;
+			$this->assertEqual($result, $expected);
 		}
 
-	public function testLoad() {
-		
-		//* test loading settings from DB
-		$result = $this->Setting->load();
-		$expected	= $this->settingsCompact;
-		$this->assertEqual($result, $expected);
+		public function testLoad() {
 
-		//* test writing settings into app-config
-		Configure::write('Saito.Settings', NULL);
-		$this->Setting->load();
-		$result = Configure::read('Saito.Settings');
-		$expected	= $this->settingsCompact;
-		$this->assertEqual($result, $expected);
-		
-		//* test caching
-		$debugState = Configure::read('debug');
-		Configure::write('debug', 0);
-		$cacheDisable = Configure::read('Cache.disable');
-		Configure::write('Cache.disable', false);
+			//* test loading settings from DB
+			$result = $this->Setting->load();
+			$expected = $this->settingsCompact;
+			$this->assertEqual($result, $expected);
 
-		$this->Setting->load();
-		$result = Cache::read('Saito.Settings');
+			//* test writing settings into app-config
+			Configure::write('Saito.Settings', NULL);
+			$this->Setting->load();
+			$result = Configure::read('Saito.Settings');
+			$expected = $this->settingsCompact;
+			$this->assertEqual($result, $expected);
 
-		Cache::delete('Saito.Settings');	
-		Configure::write('debug', $debugState);
-		Configure::write('Cache.disable', $cacheDisable);
+			//* test caching
+			$debugState = Configure::read('debug');
+			Configure::write('debug', 0);
+			$cacheDisable = Configure::read('Cache.disable');
+			Configure::write('Cache.disable', false);
 
-		$expected	= $this->settingsCompact;
-		$this->assertEqual($result, $expected);
+			$this->Setting->load();
+			$result = Cache::read('Saito.Settings');
+
+			Cache::delete('Saito.Settings');
+			Configure::write('debug', $debugState);
+			Configure::write('Cache.disable', $cacheDisable);
+
+			$expected = $this->settingsCompact;
+			$this->assertEqual($result, $expected);
 		}
 
-	function startTest($message) {
-		$this->Setting =& ClassRegistry::init('Setting');
+		public function setUp() {
+			$this->Setting = ClassRegistry::init('Setting');
+		}
+
 	}
 
-	function endTest() {
-		unset($this->Setting);
-		ClassRegistry::flush();
-	}
-}
 ?>
