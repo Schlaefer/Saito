3d8b02e1f943d794e24232583fd7fb9de07a5f7d
Schlaefer <openmail+sourcecode@siezi.com>
2012-02-18 15:21:11 +0100

    migrated smiley model test case

--- app/Test/Case/Model/SmileyTest.php
+++ app/Test/Case/Model/SmileyTest.php
@@ -1,54 +1,46 @@
 <?php
-/* Smily Test cases generated on: 2011-04-28 21:04:11 : 1304017691*/
-App::import('Model', 'Smily');
 
-class SmileyTest extends CakeTestCase {
-	var $fixtures = array('app.smiley', 'app.smiley_code');
+	App::uses('Smily', 'Model');
 
-	public function testLoad() {
+	class SmileyTest extends CakeTestCase {
 
-		// test loading into Configure
-		$this->Smiley->load();
-		$result = Configure::read('Saito.Smilies.smilies_all');
-		$expected = array(
-				array(
-					'order'			=> 1,
-					'icon'			=> 'wink.png',
-					'image'			=> 'wink.png',
-					'title'			=> 'Wink',
-					'code'			=> ';)',
-				),
-				array(
-					'order'			=> 2,
-					'icon'			=> 'smile_icon.png',
-					'image'			=> 'smile_image.png',
-					'title'			=> 'Smile',
-					'code'			=> ':-)',
-				),
-				array(
-					'order'			=> 2,
-					'icon'			=> 'smile_icon.png',
-					'image'			=> 'smile_image.png',
-					'title'			=> 'Smile',
-					'code'			=> ';-)',
-				),
+		public $fixtures = array( 'app.smiley', 'app.smiley_code' );
+
+		public function testLoad() {
+
+			// test loading into Configure
+			$this->Smiley->load();
+			$result = Configure::read('Saito.Smilies.smilies_all');
+			$expected = array(
+					array(
+							'order' => 1,
+							'icon' => 'wink.png',
+							'image' => 'wink.png',
+							'title' => 'Wink',
+							'code' => ';)',
+					),
+					array(
+							'order' => 2,
+							'icon' => 'smile_icon.png',
+							'image' => 'smile_image.png',
+							'title' => 'Smile',
+							'code' => ':-)',
+					),
+					array(
+							'order' => 2,
+							'icon' => 'smile_icon.png',
+							'image' => 'smile_image.png',
+							'title' => 'Smile',
+							'code' => ';-)',
+					),
 			);
-		$this->assertEqual($result, $expected);		
+			$this->assertEqual($result, $expected);
 		}
 
-	function startTest($message) {
-		if(php_sapi_name() == 'cli') {
-			echo "Starting ".get_class($this)."->$message()\n";
-		} else {
-			echo "<h3>Starting ".get_class($this)."->$message()</h3>";
+		function setUp() {
+			$this->Smiley = ClassRegistry::init('Smiley');
 		}
-		
-		$this->Smiley =& ClassRegistry::init('Smiley');
+
 	}
 
-	function endTest() {
-		unset($this->Smily);
-		ClassRegistry::flush();
-	}
-}
 ?>
