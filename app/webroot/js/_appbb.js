613d2e1db7b12a7c38daec3dc08941ba8e00510d
Schlaefer <openmail+sourcecode@siezi.com>
2012-02-24 12:07:08 +0100

    bootstrap init

---
+++ app/webroot/js/_appbb.js
@@ -0,0 +1,95 @@
+(function ($) {
+
+	var Thread = Backbone.Model.extend({
+
+		defaults: {
+			isThreadCollapsed: false
+		},
+
+		toggleCollapseThread: function() {
+			this.set({ 
+				isThreadCollapsed: !this.get('isThreadCollapsed')
+			});
+		}
+
+	});
+
+	var ThreadCollection = Backbone.Collection.extend({
+		model: Thread,
+		localStorage: new Store('Threads')
+	})
+
+	var ThreadView = Backbone.View.extend({
+
+		className: 'thread_box',
+
+		events: {
+			"click .btn-threadCollapse":  "collapseThread"
+		},
+
+		initialize: function(){
+			this.model.on('change:isThreadCollapsed', this.toggleCollapseThread, this);
+
+			if (this.model.get('isThreadCollapsed')) {
+				this.hide();
+			}
+		},
+
+		collapseThread: function(event) {
+			event.preventDefault();
+			this.model.toggleCollapseThread();
+			this.model.save();
+		},
+
+		toggleCollapseThread: function(model, isThreadCollapsed) {
+			if(isThreadCollapsed) {
+				this.slideUp();
+			} else {
+				this.slideDown();
+			}
+		},
+
+		slideUp: function() {
+			$(this.el).find('.tree_thread > ul > li:not(:first-child)').slideUp('100');
+		},
+
+		slideDown: function() {
+			$(this.el).find('.tree_thread > ul > li:not(:first-child)').slideDown('100');
+		},
+
+		hide: function() {
+			$(this.el).find('.tree_thread > ul > li:not(:first-child)').hide();
+		}
+
+	});
+
+	/*
+	var AppView = Backbone.View.extend({
+		el: $("body"),
+		events: {
+			"click .foobar":  "showPrompt"
+		},
+		showPrompt: function () {
+			var friend_name = prompt("Who is your friend?");
+		}
+	});
+	var appview = new AppView;
+	 */
+
+	var threads = new ThreadCollection;
+	threads.fetch();
+
+	$('.thread_box').each(function(element) {
+		var threadId = parseInt($(this).attr('data-id'));
+		if (!threads.get(threadId)) {
+			threads.add(new Thread({
+				id: threadId
+			}));
+		}
+		new ThreadView({
+			el: $(this),
+			model: threads.get(threadId)
+		});
+	});
+})(jQuery);
+
