dcd2dd18eee2bd1edbe7ab9db9d40a7bb5aa57ec
Schlaefer <openmail+sourcecode@siezi.com>
2012-03-31 20:23:06 +0200

    some explicit public methods

--- app/Lib/SaitoCacheTree.php
+++ app/Lib/SaitoCacheTree.php
@@ -9,7 +9,7 @@ class SaitoCacheTree extends Object {
 	static protected $_forceNoCache		= false;
 	static protected $_isUpdated			= FALSE; 
 
-	function canUseCache($entry = null, $user = null) {
+	public function canUseCache($entry = null, $user = null) {
 		if(self::$_forceNoCache) return false;
 		if(empty($entry)) return false;
 
@@ -43,7 +43,7 @@ class SaitoCacheTree extends Object {
 		self::$_isUpdated = TRUE;
 	}
 
-	function read($id = null) {
+	public function read($id = null) {
 		if(self::$_forceNoCache) return false;
 		if ($id === null) {
 			return self::$_cachedEntries;
@@ -56,19 +56,19 @@ class SaitoCacheTree extends Object {
 		}
 	}
 
-	function update($id, $content) {
+	public function update($id, $content) {
 		if(self::$_forceNoCache) return false;
 		$data = array('time' => time(), 'content' => $content);
 		self::$_cachedEntries[$id] = $data;
 		self::$_isUpdated = TRUE;
 	}
 
-	function readCache() {
+	public function readCache() {
 		if(self::$_forceNoCache) return false;
 		self::$_cachedEntries = Cache::read('EntrySub');
 	}
 	
-	function saveCache() {
+	public function saveCache() {
 		if( self::$_forceNoCache || self::$_isUpdated === FALSE ) return false;
 		$this->_gc();
 		self::$_cachedEntries['last_update']['day'] = mktime(0, 0, 0);
