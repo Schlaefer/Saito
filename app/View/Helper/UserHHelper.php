efbb22676f3309abea0cc21a8a463c934d41994e
Schlaefer <openmail+sourcecode@siezi.com>
2012-04-08 10:20:47 +0200

    code formating

--- app/View/Helper/UserHHelper.php
+++ app/View/Helper/UserHHelper.php
@@ -1,66 +1,66 @@
 <?php
 
-App::import('Lib', 'SaitoUser');
-App::uses('AppHelper', 'View/Helper');
+	App::import('Lib', 'SaitoUser');
+	App::uses('AppHelper', 'View/Helper');
 
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+	/*
+	 * To change this template, choose Tools | Templates
+	 * and open the template in the editor.
+	 */
 
-class UserHHelper extends AppHelper {
-	public $helpers = array ( 
-			#App
+	class UserHHelper extends AppHelper {
 
-			#CakePHP
-			'Html',
-			'Session',);
-	/**
-	 * generates the JavaSript commands to format the views according to user prefs
-	 *
-	 * @return string JavaScript commands
-	 **/
-	public function generateCss($User = NULL) {
-			$out = array();
+		public $helpers = array(
+				#App
+				#CakePHP
+				'Html',
+				'Session', );
+
+		/**
+		 * generates the JavaSript commands to format the views according to user prefs
+		 *
+		 * @return string JavaScript commands
+		 * */
+		public function generateCss($User = NULL) {
+			$out = array( );
 			$out[] = "<style type='text/css'>";
 
-			if (isset($User['id'])) {
+			if ( isset($User['id']) ) {
 
-					$font_size = $User['user_font_size'];
-					if ($font_size != 0)
-					{
-						$font_size = -1 * (1-$font_size)*20 + 12;
-						if (!empty($font_size)) {
-								$out[] = "body { font-size:". $font_size ."px; }";
-								// scales the the following parameter to a subjective nicer view
-								// @td set generaly to 1 1/3 em of font size ?
-								 $line_height = number_format( (1 + 1/2) * $font_size, 3, '.', '');
-								 $out[] =  "li { line-height:{$line_height}px; }";
+				$font_size = $User['user_font_size'];
+				if ( $font_size != 0 ) {
+					$font_size = -1 * ( 1 - $font_size ) * 20 + 12;
+					if ( !empty($font_size) ) {
+						$out[] = "body { font-size:" . $font_size . "px; }";
+						// scales the the following parameter to a subjective nicer view
+						// @td set generaly to 1 1/3 em of font size ?
+						$line_height = number_format((1 + 1 / 2) * $font_size, 3, '.', '');
+						$out[] = "li { line-height:{$line_height}px; }";
 //								$out[] =  "li { line-height: 1.45; }";
-								 $out[] =  "ul.thread {margin-bottom:{$line_height}px ;}";
-						}
+						$out[] = "ul.thread {margin-bottom:{$line_height}px ;}";
 					}
+				}
 
-					$c_new					= $User['user_color_new_postings'];
-					$c_old					= $User['user_color_old_postings'];
-					$c_act					= $User['user_color_actual_posting'];
+				$c_new = $User['user_color_new_postings'];
+				$c_old = $User['user_color_old_postings'];
+				$c_act = $User['user_color_actual_posting'];
 
-					$a_metatags     = array ( 'link', 'visited', 'hover', 'active' );
-					foreach ($a_metatags as $a_metatag) {
-							if (!empty($c_old) && $c_old !== '#') {
-									$out[] = "li.thread .span_post_type:$a_metatag, li.reply .span_post_type:$a_metatag	{ color: $c_old; }";
-							}
-							if (!empty($c_new) && $c_new !== '#') {
-									$out[] = "li.threadnew .span_post_type:$a_metatag, li.replynew .span_post_type:$a_metatag { color: $c_new; }";
-							}
-							if (!empty($c_act) && $c_act !== '#') {
-									$out[] = "li.actthread .span_post_type:$a_metatag, li.actreply .span_post_type:$a_metatag 	{ color: $c_act; }";
-							}
+				$a_metatags = array( 'link', 'visited', 'hover', 'active' );
+				foreach ( $a_metatags as $a_metatag ) {
+					if ( !empty($c_old) && $c_old !== '#' ) {
+						$out[] = "li.thread .span_post_type:$a_metatag, li.reply .span_post_type:$a_metatag	{ color: $c_old; }";
 					}
+					if ( !empty($c_new) && $c_new !== '#' ) {
+						$out[] = "li.threadnew .span_post_type:$a_metatag, li.replynew .span_post_type:$a_metatag { color: $c_new; }";
+					}
+					if ( !empty($c_act) && $c_act !== '#' ) {
+						$out[] = "li.actthread .span_post_type:$a_metatag, li.actreply .span_post_type:$a_metatag 	{ color: $c_act; }";
+					}
+				}
 			}
 			$out[] = "</style>";
 			return implode(" ", $out);
-	}
+		}
 
 	/**
 	 * If input is text and empty return minus.
