a560982187eae2913a2c6af7e9156e1280c1af64
Schlaefer <openmail+sourcecode@siezi.com>
2012-08-23 16:28:05 +0200

    moved ThreadView to own js file

--- app/webroot/js/views/app.js
+++ app/webroot/js/views/app.js
@@ -5,54 +5,12 @@ define([
 	'collections/threadlines',
 	'views/threadlines',
 	'collections/threads',
-	], function($, _, Backbone, ThreadLineCollection, ThreadLineView, ThreadCollection) {
-		var ThreadView = Backbone.View.extend({
+	'views/threads',
+	], function(
+		$, _, Backbone,
+		ThreadLineCollection, ThreadLineView,
+		ThreadCollection, ThreadView) {
 
-			className: 'thread_box',
-
-			events: {
-				"click .btn-threadCollapse":  "collapseThread"
-			},
-
-			initialize: function(){
-				this.model.on('change:isThreadCollapsed', this.toggleCollapseThread, this);
-
-				if (this.model.get('isThreadCollapsed')) {
-					this.hide();
-				}
-			},
-
-			collapseThread: function(event) {
-				event.preventDefault();
-				this.model.toggleCollapseThread();
-				this.model.save();
-			},
-
-			toggleCollapseThread: function(model, isThreadCollapsed) {
-				if(isThreadCollapsed) {
-					this.slideUp();
-				} else {
-					this.slideDown();
-				}
-			},
-
-			slideUp: function() {
-				$(this.el).find('.tree_thread > ul > li:not(:first-child)').slideUp('100');
-				$(this.el).find('.ico-threadCollapse').removeClass('ico-threadCollapse').addClass('ico-threadOpen');
-			},
-
-			slideDown: function() {
-				$(this.el).find('.tree_thread > ul > li:not(:first-child)').slideDown('100');
-				$(this.el).find('.ico-threadOpen').removeClass('ico-threadOpen').addClass('ico-threadCollapse');
-			},
-
-			hide: function() {
-				$(this.el).find('.tree_thread > ul > li:not(:first-child)').hide();
-				$(this.el).find('.ico-threadCollapse').removeClass('ico-threadCollapse').addClass('ico-threadOpen');
-			}
-		});
-
-		//	})(jQuery);
 		var threads = new ThreadCollection;
 		threads.fetch();
 
