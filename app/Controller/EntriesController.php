Schlaefer/Saito

commit b28e8de71dbd6f8f45909caa374dfa5c7aa74c3e

search for username searches User.username instead of Entry.name

app/Controller/EntriesController.php
@@ -562,7 +562,7 @@ class EntriesController extends AppController {
 					$this->paginate = array(
 							'fields' => "*, (MATCH (Entry.subject) AGAINST ('$searchTerm' IN BOOLEAN MODE)*100) + (MATCH (Entry.text) AGAINST ('$searchTerm' IN BOOLEAN MODE)*10) + MATCH (Entry.name) AGAINST ('$searchTerm' IN BOOLEAN MODE) AS rating",
 							'conditions' => array(
-                "MATCH (Entry.subject, Entry.text, Entry.name) AGAINST ('$searchTerm' IN BOOLEAN MODE)",
+                "MATCH (Entry.subject, Entry.text, User.username) AGAINST ('$searchTerm' IN BOOLEAN MODE)",
                 'Entry.category' => $this->Entry->Category->getCategoriesForAccession($this->CurrentUser->getMaxAccession())),
 							'order' => 'rating DESC, `Entry`.`time` DESC',
 							'limit' => 25,

commit bac0ff2601e63b1f6bd2b52fe86e60e6147ee3bf

[fix] search performance regression introduced in b28e8de71dbd6f8f45909caa374dfa5c7aa74c3e

app/Controller/EntriesController.php
@@ -562,7 +562,7 @@ class EntriesController extends AppController {
 					$this->paginate = array(
 							'fields' => "*, (MATCH (Entry.subject) AGAINST ('$searchTerm' IN BOOLEAN MODE)*100) + (MATCH (Entry.text) AGAINST ('$searchTerm' IN BOOLEAN MODE)*10) + MATCH (Entry.name) AGAINST ('$searchTerm' IN BOOLEAN MODE) AS rating",
 							'conditions' => array(
-                "MATCH (Entry.subject, Entry.text, User.username) AGAINST ('$searchTerm' IN BOOLEAN MODE)",
+                "MATCH (Entry.subject, Entry.text, Entry.name) AGAINST ('$searchTerm' IN BOOLEAN MODE)",
                 'Entry.category' => $this->Entry->Category->getCategoriesForAccession($this->CurrentUser->getMaxAccession())),
 							'order' => 'rating DESC, `Entry`.`time` DESC',
 							'limit' => 25,