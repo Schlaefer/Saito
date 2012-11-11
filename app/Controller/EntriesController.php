e46ad38fddcb0eeb8a278508bad175ba95326b41
Schlaefer <openmail+sourcecode@siezi.com>
2012-11-11 12:19:11 +0100

    simple search words are now mandatory

--- app/Controller/EntriesController.php
+++ app/Controller/EntriesController.php
@@ -574,10 +574,11 @@ class EntriesController extends AppController {
 
 				if ( $searchTerm ) {
           $searchTerm = Sanitize::escape($searchTerm);
+					$internal_search_term = preg_replace('/(^|\s)(?!-)/i', ' +', $searchTerm);
 					$this->paginate = array(
-							'fields' => "*, (MATCH (Entry.subject) AGAINST ('$searchTerm' IN BOOLEAN MODE)*2) + (MATCH (Entry.text) AGAINST ('$searchTerm' IN BOOLEAN MODE)) + (MATCH (Entry.name) AGAINST ('$searchTerm' IN BOOLEAN MODE)*4) AS rating",
+							'fields' => "*, (MATCH (Entry.subject) AGAINST ('$internal_search_term' IN BOOLEAN MODE)*2) + (MATCH (Entry.text) AGAINST ('$internal_search_term' IN BOOLEAN MODE)) + (MATCH (Entry.name) AGAINST ('$internal_search_term' IN BOOLEAN MODE)*4) AS rating",
 							'conditions' => array(
-                "MATCH (Entry.subject, Entry.text, Entry.name) AGAINST ('$searchTerm' IN BOOLEAN MODE)",
+                "MATCH (Entry.subject, Entry.text, Entry.name) AGAINST ('$internal_search_term' IN BOOLEAN MODE)",
                 'Entry.category' => $this->Entry->Category->getCategoriesForAccession($this->CurrentUser->getMaxAccession())),
 							'order' => 'rating DESC, `Entry`.`time` DESC',
 							'limit' => 25,
