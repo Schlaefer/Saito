06b2d239e292eee8b7aa6cbd7de762e911e9646e
Schlaefer <openmail+sourcecode@siezi.com>
2012-11-13 15:15:45 +0100

    refactored and simplified Tree behavior

--- app/Model/Behavior/TreeBehavior.php
+++ app/Model/Behavior/TreeBehavior.php
@@ -3,33 +3,33 @@
 	class TreeBehavior extends ModelBehavior {
 
 		public function treeBuild(Model $Model, $threads) {
-			$out = $this->_parseTreeInit($Model, $threads);
-			$out = $this->_sortTime($out);
-			return $out;
-		}
-
-		protected function _parseTreeInit(Model $Model, $threads) {
 			$tree = array();
 			foreach ($threads as $thread) {
-				$this->_parseTreeRecursive($Model, $tree, $thread);
+				$id = $thread[$Model->alias]['id'];
+				$pid = $thread[$Model->alias]['pid'];
+				$tree[$id] = isset($tree[$id]) ? $thread + $tree[$id] : $thread;
+				$tree[$pid]['_children'][] = &$tree[$id];
 			}
-			return $tree[0]['_children'];
+
+			$sorted_trees = $this->_sortTreesAfterTime($tree[0]['_children']);
+
+			return $sorted_trees;
 		}
 
-		protected function _parseTreeRecursive(Model $Model, &$tree, $item) {
-			$id = $item[$Model->alias]['id'];
-			$pid = $item[$Model->alias]['pid'];
-			$tree[$id] = isset($tree[$id]) ? $item + $tree[$id] : $item;
-			$tree[$pid]['_children'][] = &$tree[$id];
-		}
-
-		protected function _sortTime($in, $level = 0) {
+		/**
+		 * Sort all entries in trees after time
+		 *
+		 * @param array $in array with trees
+		 * @param int $level
+		 * @return array
+		 */
+		protected function _sortTreesAfterTime($in, $level = 0) {
 			if ($level > 0) {
 				$in = $this->_quicksort($in);
 			}
 			foreach ($in as $k => $v) {
 				if (isset($v['_children'])) {
-					$in[$k]['_children'] = $this->_sortTime($v['_children'], $level + 1);
+					$in[$k]['_children'] = $this->_sortTreesAfterTime($v['_children'], $level + 1);
 				}
 			}
 			return $in;
