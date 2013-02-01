7aded6d2542ebccf9b933d336b10e648855dd6c3
Schlaefer <openmail+sourcecode@siezi.com>
2013-02-01 12:09:44 +0100

    refactored existing js tests to jasmine.js

--- app/View/Tools/test_js.ctp
+++ app/View/Tools/test_js.ctp
@@ -1,42 +1,94 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-	<title>QUnit Test Suite</title>
-	<!-- Include QUnit files -->
-	<link rel="stylesheet" href="http://code.jquery.com/qunit/qunit-git.css" type="text/css" media="screen">
-	<script type="text/javascript" src="http://code.jquery.com/qunit/qunit-git.js"></script>
-	<!-- Setup project environment -->
-	<script type="text/javascript">
-		var SaitoApp = {
-			settings: {
-				embedly_enabled: '1'
-			},
-			currentUser: {
-				user_show_inline: '0'
-			}
-		};
-	</script>
-	<!-- Include project files -->
-	<?php
-	  echo $this->Html->script(
-			array(
-				'lib/jquery/jquery-1.9.0.js',
-				'lib/jquery-ui/jquery-ui-1.9.2.custom.min.js',
-				'bootstrap/bootstrap.js',
-				'classes/thread_line.class.js',
-				'_app.js',
-				'lib/jquery.scrollTo/jquery.scrollTo-min.js',
-				'tests/test.js' // Include test files
-			)
-		);
-	?>
+    <title>Jasmine Spec Runner</title>
+
+    <script type="text/javascript">
+        var SaitoApp = {
+            settings: {
+                embedly_enabled: '1'
+            },
+            currentUser: {
+                user_show_inline: '0'
+            }
+        };
+    </script>
+
+    <!-- include Jasmin -->
+		<?php
+			echo $this->Html->css('lib/jasmine/jasmine.css');
+			echo $this->Html->script(
+				array(
+					'lib/jasmine/jasmine.js',
+					'lib/jasmine/jasmine-html.js'
+				)
+			);
+		?>
+
+    <!-- include libs -->
+		<?php
+			echo $this->Html->script(
+				array(
+					'lib/jquery/jquery-1.9.0.js',
+					'lib/jquery-ui/jquery-ui-1.9.2.custom.min.js',
+					'bootstrap/bootstrap.js',
+					'classes/thread_line.class.js',
+					'_app.js',
+					'lib/jquery.scrollTo/jquery.scrollTo-min.js',
+				)
+			);
+		?>
+
+    <!-- include source -->
+		<?php
+			echo $this->Html->script(
+				array(
+					'_app.js',
+				)
+			);
+		?>
+
+    <!-- include specs -->
+		<?php
+			echo $this->Html->script(
+				array(
+					'tests/MarkItUpSpec.js'
+				)
+			);
+		?>
+
+    <script type="text/javascript">
+        (function() {
+            var jasmineEnv = jasmine.getEnv();
+            jasmineEnv.updateInterval = 1000;
+
+            var htmlReporter = new jasmine.HtmlReporter();
+
+            jasmineEnv.addReporter(htmlReporter);
+
+            jasmineEnv.specFilter = function(spec) {
+                return htmlReporter.specFilter(spec);
+            };
+
+            var currentWindowOnload = window.onload;
+
+            window.onload = function() {
+                if (currentWindowOnload) {
+                    currentWindowOnload();
+                }
+                execJasmine();
+            };
+
+            function execJasmine() {
+                jasmineEnv.execute();
+            }
+
+        })();
+    </script>
 
 </head>
+
 <body>
-	<h1 id="qunit-header">QUnit Test Suite</h1>
-	<h2 id="qunit-banner"></h2>
-	<div id="qunit-testrunner-toolbar"></div>
-	<h2 id="qunit-userAgent"></h2>
-	<ol id="qunit-tests"></ol>
 </body>
 </html>
---
+++ app/webroot/js/tests/MarkItUpSpec.js
@@ -0,0 +1,90 @@
+
+// @todo see also test_js.ctp
+var SaitoApp = {app: {settings:{embedly_enabled: 0}}};
+
+// @todo initialize markItUp and don't use a global var
+
+describe("markItUp library", function() {
+
+    describe("multimedia button", function() {
+
+        var input,
+            result,
+            expected;
+
+        $.each(['m4a', 'ogg', 'mp3', 'wav', 'opus'], function(key, value) {
+            it("outputs an [audio] tag for " + value + " files", function() {
+                input = 'http://foo.bar/baz.' + value;
+                result = markItUp.multimedia(input);
+                expected =  '[audio]http://foo.bar/baz.' + value + '[/audio]'
+                expect(result).toEqual(expected);
+            });
+        });
+
+        $.each(['mp4', 'webm', 'm4v'], function(key, value) {
+            it("outputs an [video] tag for " + value + " files", function() {
+                input = 'http://foo.bar/baz.' + value;
+                result = markItUp.multimedia(input);
+                expected =  '[video]http://foo.bar/baz.' + value + '[/video]'
+                expect(result).toEqual(expected);
+            });
+        });
+
+        it("outputs an [iframe] tag for <iframe> tags", function() {
+            input = '<iframe src="http://www.youtube.com/embed/qa-4E8ZDj9s" width="425" '
+                + 'height="349" frameborder="0" allowfullscreen></iframe>';
+            result = markItUp.multimedia(input);
+            expected = '[iframe src=http://www.youtube.com/embed/qa-4E8ZDj9s width=425 '
+                + 'height=349 frameborder=0 allowfullscreen][/iframe]';
+            expect(result).toEqual(expected);
+        });
+
+        it("outputs an [flash_video] tag for <object> tags", function() {
+            input = '<object … src="http://www.youtube.com/v/qa-4E8ZDj9s?version=3&amp;hl=en_US" … width="425" height="349" …'
+            result = markItUp.multimedia(input);
+            expected = '[flash_video]http://www.youtube.com/v/qa-4E8ZDj9s?version=3&amp;hl=en_US|425|349[/flash_video]';
+        });
+
+        it("outputs an [iframe] tag for a raw youtube url", function() {
+            input = 'http://www.youtube.com/watch?v=qa-4E8ZDj9s'
+            result = markItUp.multimedia(input);
+            expected = '[iframe src=http://www.youtube.com/embed/qa-4E8ZDj9s width=425 '
+                + 'height=349 frameborder=0 allowfullscreen][/iframe]';
+            expect(result).toEqual(expected);
+        });
+
+        it("outputs an [iframe] tag for a raw youtube url without protocol", function() {
+            input = 'www.youtube.com/watch?v=0u8KUgUqprw';
+            result = markItUp.multimedia(input);
+            expected = '[iframe src=http://www.youtube.com/embed/0u8KUgUqprw width=425 '
+                + 'height=349 frameborder=0 allowfullscreen][/iframe]';
+            expect(result).toEqual(expected);
+        });
+
+        it("outputs an [iframe] tag for youtu.be url shortener ", function() {
+            input = 'http://youtu.be/qa-4E8ZDj9s';
+            result = markItUp.multimedia(input);
+            expected = '[iframe src=http://www.youtube.com/embed/qa-4E8ZDj9s width=425 '
+                + 'height=349 frameborder=0 allowfullscreen][/iframe]';
+            expect(result).toEqual(expected);
+        });
+
+        it("outputs nothing for embed.ly if embed.ly is disabled", function() {
+            SaitoApp.app.settings.embedly_enabled = 0;
+            input = 'https://twitter.com/apfelwiki/status/211385090444505088';
+            result = markItUp.multimedia(input);
+            expected = '';
+            expect(result).toEqual(expected);
+        });
+
+        it("outputs [embed] tag to use embed.ly as fallback", function() {
+            SaitoApp.app.settings.embedly_enabled = 1;
+            input = 'https://twitter.com/apfelwiki/status/211385090444505088';
+            result = markItUp.multimedia(input);
+            expected = '[embed]' + input + '[/embed]';
+            expect(result).toEqual(expected);
+        });
+
+    });
+
+});
