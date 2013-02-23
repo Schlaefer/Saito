210693d395218891f1f1c190236886558f5655a4
Schlaefer <openmail+sourcecode@siezi.com>
2013-02-23 10:34:29 +0100

    make cakeRest amd safe

--- app/webroot/js/lib/saito/backbone.cakeRest.js
+++ app/webroot/js/lib/saito/backbone.cakeRest.js
@@ -1,5 +1,12 @@
-define([
-], function() {
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(["underscore","backbone"], function(_, Backbone) {
+            return factory(_ || root._, Backbone || root.Backbone);
+        });
+    } else {
+        factory(_, Backbone);
+    }
+})(this, function(_, Backbone) {
 
     SaitoApp.Mixins = SaitoApp.Mixins || {};
 
