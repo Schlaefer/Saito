24ef998caf5bfc56653a58e8c6e3081047d1b778
Schlaefer <openmail+sourcecode@siezi.com>
2013-03-10 16:09:13 +0100

    code formatting

--- app/Model/Upload.php
+++ app/Model/Upload.php
@@ -15,15 +15,16 @@ class Upload extends AppModel {
 
 	public $belongsTo = array('User');
 
-  public function deleteAllFromUser($user_id) {
-    return $this->deleteAll(
-          array(
-              'Upload.user_id' => $user_id),
-              FALSE,
-              // call beforeDelete FileUploader plugin callback to remove files from disk
-              TRUE
-          );
-  }
+	public function deleteAllFromUser($user_id) {
+		return $this->deleteAll(
+			array(
+				'Upload.user_id' => $user_id
+			),
+			false,
+			// call beforeDelete FileUploader plugin callback to remove files from disk
+			true
+		);
+	}
 
 
 }
