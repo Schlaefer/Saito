72f0d4ea9c3cd6b350beedf797718e70f09bd130
Schlaefer <openmail+sourcecode@siezi.com>
2013-03-19 15:20:45 +0100

    fixed login/logout selenium test

--- app/Test/Selenium/loginAndLogoutTest.php
+++ app/Test/Selenium/loginAndLogoutTest.php
@@ -1,46 +1,33 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+	require_once '_saitoSelenium.php';
 
-require_once '_saitoSelenium.php';
+	class LoginAndLogoutTest extends Saito_SeleniumTestCase {
 
-// require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+		function testLoginAndLogoutWithoutCookie() {
+			$this->login($this);
+			$this->logout($this);
+		}
 
-/**
- * Description of newSeleneseTest
- *
- * @author siezi
- */
-class loginAndLogoutTest extends Saito_SeleniumTestCase {
+		public function testLoginAndLogoutWithCookie() {
+			$cookie_name = "SaitoPersistent[AU]";
 
-	function setUp() {
-		parent::setUp();
+			$this->open();
+			$this->waitForPageToLoad();
+			$this->click("showLoginForm");
+			$this->type("tf-login-username", $this->userName);
+			$this->type("UserPassword", $this->userPassword);
+			$this->click("UserRememberMe");
+			$this->click("//input[@value='Login']");
+			$this->waitForPageToLoad("30000");
+			$this->assertTrue(
+				(bool)preg_match(
+					'/^[\s\S]*$/',
+					$this->getCookieByName($cookie_name)
+				)
+			);
+			$this->logout($this);
+			$this->assertFalse($this->isCookiePresent($cookie_name));
+		}
 
-		// $this->setSleep(1);
 	}
-
-	function testLoginAndLogoutWithoutCookie() {
-		$this->login($this);
-		$this->logout($this);
-		}
-
-	public function testLoginAndLogoutWithCookie() {
-		$this->open();
-    $this->assertEquals("0", $this->getElementHeight("modalLoginDialog"));
-    $this->click("showLoginForm");
-    $this->waitForPageToLoad("");
-    $this->assertNotEquals("0", $this->getElementHeight("modalLoginDialog"));
-		$this->type("tf-login-username", $this->userName);
-		$this->type("UserPassword", $this->userPassword);
-    $this->click("UserRememberMe");
-    $this->click("//input[@value='Login']");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue((bool)preg_match('/^[\s\S]*$/',$this->getCookieByName("SaitoPersistent[AU]")));
-		$this->logout($this);
-		$this->assertFalse($this->isCookiePresent("macnemo[AU]"));
-		}
-	
-}
