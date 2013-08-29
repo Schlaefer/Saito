1778d45cfe163fe076e7ce8776d76dbdc9f0f729
Schlaefer <openmail+sourcecode@siezi.com>
2013-08-29 11:34:28 +0200

    improved spoiler tag

--- app/View/Helper/BbcodeHelper.php
+++ app/View/Helper/BbcodeHelper.php
@@ -762,11 +762,11 @@ class BbcodeHelper extends AppHelper implements MarkupParser {
 			'/\[spoiler\](.*).?\[\/spoiler\]/',
 			function ($matches) {
 				$length = mb_strlen(strip_tags($matches[1]));
-				$minLenght = mb_strlen(__('Spoiler')) + 4;
+				$minLenght = mb_strlen(__('Spoiler')) + 6;
 				if ($length < $minLenght) {
 					$length = $minLenght;
 				}
-				$title = $this->mb_strpad(' ' . __('Spoiler') . ' ', $length, '▇', STR_PAD_BOTH);
+				$title = '▇ ' . __('Spoiler') . ' ▇';
 				$json  = json_encode(['string' => $matches[1]]);
 				$id = 'spoiler_' . rand(0, 9999999999999);
 				$out = <<<EOF
@@ -774,7 +774,7 @@ class BbcodeHelper extends AppHelper implements MarkupParser {
 	<script>
 		window.$id = $json;
 	</script>
-	<a href="#" class="c_bbc_spoiler-link"
+	<a href="#" class="c_bbc_spoiler-link" style="width: {$length}ex; display: inline-block; text-align: center;"
 		onclick='this.parentNode.innerHTML = window.$id.string; delete window.$id; return false;'
 		>
 		$title
@@ -1108,38 +1108,6 @@ EOF;
 			$string = str_replace($token['replacement'], $token['original'], $string);
 		}
 		return $string;
-	}
-
-	/**
-	 * @see http://www.php.net/manual/en/function.str-pad.php#111147
-	 */
-	protected function mb_strpad($str, $pad_len, $pad_str = ' ', $dir = STR_PAD_RIGHT) {
-		$str_len = mb_strlen($str);
-		$pad_str_len = mb_strlen($pad_str);
-		if (!$str_len && ($dir == STR_PAD_RIGHT || $dir == STR_PAD_LEFT)) {
-			$str_len = 1; // @debug
-		}
-		if (!$pad_len || !$pad_str_len || $pad_len <= $str_len) {
-			return $str;
-		}
-
-		$result = null;
-		$repeat = ceil($str_len - $pad_str_len + $pad_len);
-		if ($dir == STR_PAD_RIGHT) {
-			$result = $str . str_repeat($pad_str, $repeat);
-			$result = mb_substr($result, 0, $pad_len);
-		} else if ($dir == STR_PAD_LEFT) {
-			$result = str_repeat($pad_str, $repeat) . $str;
-			$result = mb_substr($result, -$pad_len);
-		} else if ($dir == STR_PAD_BOTH) {
-			$length = ($pad_len - $str_len) / 2;
-			$repeat = ceil($length / $pad_str_len);
-			$result = mb_substr(str_repeat($pad_str, $repeat), 0, floor($length))
-					. $str
-					. mb_substr(str_repeat($pad_str, $repeat), 0, ceil($length));
-		}
-
-		return $result;
 	}
 }
 
