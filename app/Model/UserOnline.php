5853cad3683a511a4317310866da0d3bb876f227
Schlaefer <openmail+sourcecode@siezi.com>
2013-09-27 10:40:55 +0200

    code formatting

--- app/Model/UserOnline.php
+++ app/Model/UserOnline.php
@@ -1,125 +1,119 @@
 <?php
 
-/**
- *  Class UserOnline
- */
-class UserOnline extends AppModel {
-	public  $name = 'UserOnline';
-	public  $useTable = 'useronline';
-	public  $primaryKey	= 'user_id';
- 	public  $actsAs = array('Containable');
-
-	public  $belongsTo = array (
-		'User' => array (
-				'className' => 'User',
-				'foreignKey'	=> 'user_id',
-		)
-	);
-
-	public $validation = array(
-			'user_id'	=> array(),
-	);
-
 	/**
-	 * Time in seconds until a user is considered offline
-	 * 
-	 * @var int
+	 *  Class UserOnline
 	 */
-	public $timeUntilOffline  = 1200;
+	class UserOnline extends AppModel {
+		public $name = 'UserOnline';
+		public $useTable = 'useronline';
+		public $primaryKey = 'user_id';
+		public $actsAs = ['Containable'];
 
-	public function beforeValidate($options = array()) {
-		parent::beforeValidate($options);
-
-		// @mlf use created/modified
-		$this->data['UserOnline']['time'] = time();
-	}
-
-	/**
-	 * Sets user `$id` online
-	 *
-	 * The `$delete_id` is handy if a user logs in or out:
-	 * We can remove his IP before setting the uid_<user_id> and vice versa.
-	 *
-	 * @param string $id `user_id` from table `User` or IP address
-	 * @param bool $loggedIn user is logged-in
-	 */
-	public function setOnline($id, $logged_in = null) {
-//		Stopwatch::start('Model->UserOnline->setOnline()');
-
-		if (empty($id)) {
-			throw new InvalidArgumentException('Invalid Argument in setOnline()');
-		}
-		if ($logged_in === null) {
-			throw new InvalidArgumentException('Invalid Argument $loggedIn in setOnline()');
-		}
-
-		$this->id = $this->_getShortendedId($id);
-
-		//* setup data
-		$data = [
-			'UserOnline' => [
-				'user_id'   => $this->id,
-				'logged_in' => $logged_in
+		public $belongsTo = [
+			'User' => [
+				'className'  => 'User',
+				'foreignKey' => 'user_id'
 			]
 		];
 
-		$this->contain();
-		$user = $this->read();
+		/**
+		 * Time in seconds until a user is considered offline
+		 *
+		 * @var int
+		 */
+		public $timeUntilOffline = 1200;
 
-		if ($user) {
-			// only hit database if timestamp is outdated
-			if ($user['UserOnline']['time'] < (time() - $this->timeUntilOffline)) {
+		public function beforeValidate($options = []) {
+			parent::beforeValidate($options);
+
+			// @mlf use created/modified
+			$this->data['UserOnline']['time'] = time();
+		}
+
+		/**
+		 * Sets user `$id` online
+		 *
+		 * The `$delete_id` is handy if a user logs in or out:
+		 * We can remove his IP before setting the uid_<user_id> and vice versa.
+		 *
+		 * @param string $id `user_id` from table `User` or IP address
+		 * @param bool $loggedIn user is logged-in
+		 */
+		public function setOnline($id, $logged_in = null) {
+
+			if (empty($id)) {
+				throw new InvalidArgumentException('Invalid Argument $id in setOnline()');
+			}
+			if ($logged_in === null) {
+				throw new InvalidArgumentException('Invalid Argument $logged_in in setOnline()');
+			}
+
+			$this->id = $this->_getShortendedId($id);
+			$data = [
+				'UserOnline' => [
+					'user_id'   => $this->id,
+					'logged_in' => $logged_in
+				]
+			];
+
+			$this->contain();
+			$user = $this->read();
+
+			if ($user) {
+				// only hit database if timestamp is outdated
+				if ($user['UserOnline']['time'] < (time() - $this->timeUntilOffline)) {
+					$this->save($data);
+				}
+			} else {
+				$this->id = null;
+				$this->create();
 				$this->save($data);
 			}
-		} else {
-			$this->id = null;
-			$this->create();
-			$this->save($data);
+
+			$this->_deleteOutdated();
 		}
 
-		$this->_deleteOutdated();
+		/**
+		 * Removes user with `$id` from UserOnline
+		 *
+		 * @param string $id
+		 */
+		public function setOffline($id) {
+			$this->id = $this->_getShortendedId($id);
+			return $this->delete($id, false);
 		}
 
-	/**
-	 * Removes user with `$id` from UserOnline
-	 *
-	 * @param string $id
-	 */
-	public function setOffline($id) {
-		$this->id = $this->_getShortendedId($id);
-		return $this->delete($id, false);
-	}
-
-	public function getLoggedIn() {
-		Stopwatch::start('UserOnline->getLoggedIn()');
-		$loggedInUsers = $this->find(
-						'all',
-						array(
-								'contain' 		=> 'User',
-								'conditions' 	=> array ('UserOnline.logged_in ='  => 1),
-								'fields'			=> 'User.id, User.username, User.user_type',
-								'order'				=> 'User.username ASC',
-						)
+		public function getLoggedIn() {
+			Stopwatch::start('UserOnline->getLoggedIn()');
+			$loggedInUsers = $this->find(
+				'all',
+				[
+					'contain'    => 'User',
+					'conditions' => ['UserOnline.logged_in =' => 1],
+					'fields'     => 'User.id, User.username, User.user_type',
+					'order'      => 'User.username ASC'
+				]
 			);
-		Stopwatch::stop('UserOnline->getLoggedIn()');
-    return $loggedInUsers;
+			Stopwatch::stop('UserOnline->getLoggedIn()');
+			return $loggedInUsers;
 		}
 
-	/**
-	 * deletes gone user
-	 *
-	 * Gone users are user who are not seen for $time_diff minutes.
-	 *
-	 * @param string $time_diff in minutes
-	 */
-	protected function _deleteOutdated($time_diff = NULL) {
-		if( $time_diff === NULL ) $time_diff = $this->timeUntilOffline;
-		$this->deleteAll( array ( 'time <' => time() - ( $time_diff )), false );
+		/**
+		 * deletes gone user
+		 *
+		 * Gone users are user who are not seen for $time_diff minutes.
+		 *
+		 * @param string $time_diff in minutes
+		 */
+		protected function _deleteOutdated($time_diff = null) {
+			if ($time_diff === null) {
+				$time_diff = $this->timeUntilOffline;
+			}
+			$this->deleteAll(['time <' => time() - ($time_diff)], false);
+		}
+
+		protected function _getShortendedId($id) {
+			return substr($id, 0, 32);
+		}
+
 	}
-
-	protected function _getShortendedId($id) {
-		return substr($id, 0, 32);
-		} 
-
-
-}
