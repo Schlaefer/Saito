eed1549495c2fbd91ec723fa62d6d9b3c06d49ae
Schlaefer <openmail+sourcecode@siezi.com>
2013-10-29 10:55:38 +0100

    removes double code

--- app/webroot/js/views/upload.js
+++ app/webroot/js/views/upload.js
@@ -3,7 +3,8 @@ define([
     'underscore',
     'backbone',
     'models/app',
-    'text!templates/upload.html'
+    'text!templates/upload.html',
+    'lib/saito/jquery.insertAtCaret'
 ], function($, _, Backbone,
             App,
             uploadTpl
@@ -45,46 +46,8 @@ define([
 
         _insert: function(event) {
             event.preventDefault();
-            this._insertAtCaret(
-                "[upload]" + this.model.get('name') + "[/upload]",
-                this.textarea
-            );
-        },
-
-        _insertAtCaret: function(text, txtarea) {
-            // var scrollPos = txtarea.scrollTop;
-            var strPos = 0,
-                range;
-            var br = ((txtarea.selectionStart || txtarea.selectionStart === '0') ?
-                "ff" : (document.selection ? "ie" : false ) );
-            if (br === "ie") {
-                txtarea.focus();
-                range = document.selection.createRange();
-                range.moveStart ('character', -txtarea.value.length);
-                strPos = range.text.length;
-            }
-            else if (br === "ff") {
-              strPos = txtarea.selectionStart;
-            }
-
-            var front = (txtarea.value).substring(0,strPos);
-            var back = (txtarea.value).substring(strPos,txtarea.value.length);
-            txtarea.value=front+text+back;
-            strPos = strPos + text.length;
-            if (br === "ie") {
-                txtarea.focus();
-                range = document.selection.createRange();
-                range.moveStart ('character', -txtarea.value.length);
-                range.moveStart ('character', strPos);
-                range.moveEnd ('character', 0);
-                range.select();
-            }
-            else if (br === "ff") {
-                txtarea.selectionStart = strPos;
-                txtarea.selectionEnd = strPos;
-                txtarea.focus();
-            }
-            // txtarea.scrollTop = scrollPos;
+            $(this.textarea).insertAtCaret(
+                "[upload]" + this.model.get('name') + "[/upload]");
         },
 
         render: function() {
