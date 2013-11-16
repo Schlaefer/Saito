972cce155a07d029f7d90078ebb993b4731d33ec
Schlaefer <openmail+sourcecode@siezi.com>
2013-11-16 09:23:07 +0100

    code formatting

--- app/webroot/js/lib/jquery.i18n/jquery.i18n.extend.js
+++ app/webroot/js/lib/jquery.i18n/jquery.i18n.extend.js
@@ -2,60 +2,61 @@
  * Extension for i18n for CakePHP/Saito
  */
 define([
-    'jquery',
-    'lib/jquery.i18n/jquery.i18n'
+  'jquery',
+  'lib/jquery.i18n/jquery.i18n'
 ], function($) {
+  'use strict';
 
-    $.extend($.i18n, {
+  $.extend($.i18n, {
 
-        currentString: '',
+    currentString: '',
 
-        setDict: function(dict) {
-           this.dict = dict;
-        },
+    setDict: function(dict) {
+      this.dict = dict;
+    },
 
-        setUrl: function(dictUrl) {
-            this.dictUrl = dictUrl;
-            this._loadDict();
-        },
+    setUrl: function(dictUrl) {
+      this.dictUrl = dictUrl;
+      this._loadDict();
+    },
 
-        _loadDict: function() {
-            return $.ajax({
-                url: this.dictUrl,
-                dataType: 'json',
-                async: false,
-                cache: true,
-                success: $.proxy(function(data) {
-                    this.dict = data;
-                }, this)
-            });
-        },
+    _loadDict: function() {
+      return $.ajax({
+        url: this.dictUrl,
+        dataType: 'json',
+        async: false,
+        cache: true,
+        success: $.proxy(function(data) {
+          this.dict = data;
+        }, this)
+      });
+    },
 
-        /**
-         * Localice string with tokens
-         *
-         * Token replacement compatible to CakePHP's String::insert()
-         *
-         */
-        __: function(string, tokens) {
-          var out = string;
-          if (typeof this.dict[string] === 'string' && this.dict[string] !== '') {
-            out = this.dict[string];
-          }
-          if (typeof tokens === 'object') {
-            out = this._insert(out, tokens);
-          }
-          return out;
-        },
+    /**
+     * Localice string with tokens
+     *
+     * Token replacement compatible to CakePHP's String::insert()
+     *
+     */
+    __: function(string, tokens) {
+      var out = string;
+      if (typeof this.dict[string] === 'string' && this.dict[string] !== '') {
+        out = this.dict[string];
+      }
+      if (typeof tokens === 'object') {
+        out = this._insert(out, tokens);
+      }
+      return out;
+    },
 
-        _insert: function(string, tokens) {
-            return string.replace(/:([-\w]+)/g, function(token, match, number, text){
-                if(typeof tokens[match] !== "undefined") {
-                    return tokens[match];
-                }
-                return token;
-            });
+    _insert: function(string, tokens) {
+      return string.replace(/:([-\w]+)/g, function(token, match, number, text) {
+        if (typeof tokens[match] !== "undefined") {
+          return tokens[match];
         }
-    });
+        return token;
+      });
+    }
+  });
 
 });
