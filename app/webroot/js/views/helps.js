66627f4c95c0c4c249bae58aac45f2518200ac21
Schlaefer <openmail+sourcecode@siezi.com>
2013-11-19 12:33:21 +0100

    fixes wonky help display

--- app/webroot/js/views/helps.js
+++ app/webroot/js/views/helps.js
@@ -1,84 +1,84 @@
 define([
-    'jquery',
-    'underscore',
-    'backbone'
+  'jquery',
+  'underscore',
+  'backbone'
 ], function($, _, Backbone) {
 
-    "use strict";
+  "use strict";
 
-    var HelpsView = Backbone.View.extend({
+  var HelpsView = Backbone.View.extend({
 
-        isHelpShown: false,
+    isHelpShown: false,
 
-        events: function() {
-            var out = {};
-            out["click " + this.indicatorName] = "toggle";
-            return out;
-        },
+    events: function() {
+      var out = {};
+      out["click " + this.indicatorName] = "toggle";
+      return out;
+    },
 
-        initialize: function(options) {
-            this.indicatorName = options.indicatorName;
-            this.elementName = options.elementName;
+    initialize: function(options) {
+      this.indicatorName = options.indicatorName;
+      this.elementName = options.elementName;
 
-            this.activateHelpButton();
-            this.placeHelp();
-        },
+      this.activateHelpButton();
+      this.placeHelp();
+    },
 
-        activateHelpButton: function() {
-            if(this.isHelpOnPage()) {
-                $(this.indicatorName).removeClass('no-color');
-            }
-        },
+    activateHelpButton: function() {
+      if (this.isHelpOnPage()) {
+        $(this.indicatorName).removeClass('no-color');
+      }
+    },
 
-        placeHelp: function() {
-            var defaults = {
-                trigger: 'manual',
-                html: true
-            };
-            var positions = ['bottom', 'right', 'left'];
-            for (var i in positions) {
-                $(this.elementName + '-' + positions[i]).popover(
-                    $.extend(defaults, {placement: positions[i]})
-                );
-            }
+    placeHelp: function() {
+      var defaults = {
+        trigger: 'manual',
+        html: true
+      };
+      var positions = ['bottom', 'right', 'left'];
+      for (var i = 0; i < positions.length; i++) {
+        $(this.elementName + '-' + positions[i]).popover(
+            $.extend(defaults, {placement: positions[i]})
+        );
+      }
 
-            $(this.indicatorName).popover({
-                placement:  'left',
-                trigger:    'manual'
-            });
-        },
+      $(this.indicatorName).popover({
+        placement: 'left',
+        trigger: 'manual'
+      });
+    },
 
-        isHelpOnPage: function() {
-            return this.$el.find(this.elementName).length > 0;
-        },
+    isHelpOnPage: function() {
+      return this.$el.find(this.elementName).length > 0;
+    },
 
-        toggle: function() {
-            event.preventDefault();
+    toggle: function(event) {
+      event.preventDefault();
 
-            if (this.isHelpShown) {
-                this.hide();
-            } else {
-                this.show();
-            }
-        },
+      if (this.isHelpShown) {
+        this.hide();
+      } else {
+        this.show();
+      }
+    },
 
 
-        show: function() {
-            this.isHelpShown = true;
-            if(this.isHelpOnPage()) {
-                $(this.elementName).popover('show');
-            } else {
-                $(this.indicatorName).popover('show');
-            }
-        },
+    show: function() {
+      this.isHelpShown = true;
+      if (this.isHelpOnPage()) {
+        $(this.elementName).popover('show');
+      } else {
+        $(this.indicatorName).popover('show');
+      }
+    },
 
-        hide: function () {
-            this.isHelpShown = false;
-            $(this.elementName).popover('hide');
-            $(this.indicatorName).popover('hide');
-        }
-    });
+    hide: function() {
+      this.isHelpShown = false;
+      $(this.elementName).popover('hide');
+      $(this.indicatorName).popover('hide');
+    }
+  });
 
-    return HelpsView;
+  return HelpsView;
 
 });
