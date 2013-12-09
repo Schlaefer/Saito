22db6a1bc7ed5b490f5c9520fa06c760786b5f0b
Schlaefer <openmail+sourcecode@siezi.com>
2013-12-09 10:10:33 +0100

    new smilies

--- app/Config/saito_config.php
+++ app/Config/saito_config.php
@@ -19,17 +19,29 @@
 
   /**
    * Add additional buttons to editor
-   */
+	 *
+	 * You can theme them with
+	 *
+	 * <code>
+	 * 	.markItUp .markItUpButton<Id> a {
+	 * 		…
+	 *	}
+	 * </code>
+   *
   /*
   Configure::write(
       'Saito.markItUp.additionalButtons',
       array(
         'Button1' => array(
-            'title'       => 'Button 1'
-            // image in img/markitup/<button>.png
-            'icon'			=> 'button1',
+  					// button-text
+  					'name' => 'Do Something',
+  					// hover title
+            'title'       => 'Button 1',
             // code inserted into text
             'code' 				=> ':action:',
+
+						// image in img/markitup/<icon-name>, replaces `name` (optional)
+						'icon'			=> 'icon-name.png', (optional)
             // format replacement as image (optional)
             'type'				=> 'image',
             // replacement in output if type is image
