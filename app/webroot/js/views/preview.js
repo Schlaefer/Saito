6c353cf1f0ce4bc86ff0a0d02d22ce44823dd4be
Schlaefer <openmail+sourcecode@siezi.com>
2014-02-10 19:40:02 +0100

    code formatting

--- app/webroot/js/views/preview.js
+++ app/webroot/js/views/preview.js
@@ -1,41 +1,41 @@
 define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'text!templates/spinner.html'
-], function ($, _, Backbone, spinnerTpl) {
+  'jquery',
+  'underscore',
+  'marionette',
+  'text!templates/spinner.html'
+], function($, _, Marionette, spinnerTpl) {
 
-    "use strict";
+  "use strict";
 
-    var PreviewView = Backbone.View.extend({
+  var PreviewView = Backbone.View.extend({
 
-        initialize: function () {
-            this.render();
+    initialize: function() {
+      this.render();
 
-            this.listenTo(this.model, "change:fetchingData", this._spinner);
-            this.listenTo(this.model, "change:rendered", this.render);
-        },
+      this.listenTo(this.model, "change:fetchingData", this._spinner);
+      this.listenTo(this.model, "change:rendered", this.render);
+    },
 
-        _spinner: function (model) {
-            if (model.get('fetchingData')) {
-                this.$el.html(spinnerTpl);
-            } else {
-                this.$el.html('');
-            }
-        },
+    _spinner: function(model) {
+      if (model.get('fetchingData')) {
+        this.$el.html(spinnerTpl);
+      } else {
+        this.$el.html('');
+      }
+    },
 
-        render: function () {
-            var rendered;
-            rendered =  this.model.get('rendered');
-            if (!rendered) {
-                rendered = '';
-            }
-            this.$el.html(rendered);
-            return this;
-        }
+    render: function() {
+      var rendered;
+      rendered = this.model.get('rendered');
+      if (!rendered) {
+        rendered = '';
+      }
+      this.$el.html(rendered);
+      return this;
+    }
 
-    });
+  });
 
-    return PreviewView;
+  return PreviewView;
 
 });
