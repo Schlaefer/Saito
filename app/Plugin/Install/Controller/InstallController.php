0c78ec214ef4eadf9d7108452130b01a846bd837
Schlaefer <openmail+sourcecode@siezi.com>
2014-02-13 15:39:10 +0100

    closes #200 Set database default connection for installation to utf8

--- app/Plugin/Install/Controller/InstallController.php
+++ app/Plugin/Install/Controller/InstallController.php
@@ -55,7 +55,7 @@ class InstallController extends InstallAppController {
 		'database'=> 'croogo',
 		'schema'=> null,
 		'prefix'=> null,
-		'encoding' => 'UTF8',
+		'encoding' => 'utf8',
 		'port' => null,
 	);
 
