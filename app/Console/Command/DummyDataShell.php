f23b340333df0234bc4e9f2402016ae6d185f866
Schlaefer <openmail+sourcecode@siezi.com>
2014-05-05 14:15:12 +0200

    create users in dummy shell

--- app/Console/Command/DummyDataShell.php
+++ app/Console/Command/DummyDataShell.php
@@ -14,12 +14,26 @@
 
 		protected $_Threads = [];
 
+		protected $_users = ['aaron', 'Alex', 'Amy', 'Ana-Lucia', 'Anthony', 'Ben',
+			'Bernard', 'Boone', 'Carmen', 'Carole', 'Charles', 'Charlie', 'Charlotte',
+			'Christian', 'Claire', 'Daniel', 'Danielle', 'Desmond', 'Dogen', 'Eko',
+			'Eloise', 'Ethan', 'Frank', 'Frogurt', 'George', 'Gina', 'Horace', 'Hugo',
+			'Ilana', 'Jack', 'Jacob', 'James', 'Jin', 'John', 'Juliet', 'Kate',
+			'Kelvin', 'Liam', 'Libby', 'Martin', 'Maninbla', 'Michael', 'Michelle',
+			'Miles', 'Nadia', 'Naomi', 'Nikki', 'Omar', 'Paulo', 'Penny', 'Pierre',
+			'Richard', 'Sarah', 'Sayid', 'Shannon', 'Stuart', 'Sun', 'Teresa', 'Tom',
+			'walt'];
+
 		public function main() {
+			$this->user();
 			$this->generate();
 		}
 
 		public function generate() {
 			$n = (int)$this->in('Number of postings to generate?', null, 100);
+			if ($n === 0) {
+				return;
+			}
 			$ratio = (int)$this->in('Average answers per thread?', null, 10);
 			$seed = $n / $ratio;
 
@@ -53,10 +67,7 @@
 					throw new RuntimeException('Could not create entry: ' . $entry);
 				}
 
-				$this->out('.', 0);
-				if ($i > 1 && !($i % 50)) {
-					$this->out(" $i/$n", 1);
-				}
+				$this->_progress($i, $n);
 
 				$id = $entry['Entry']['id'];
 				$this->_Threads[$id] = $id;
@@ -64,6 +75,47 @@
 
 			$this->out();
 			$this->out("Generated $i postings.");
+		}
+
+		public function user() {
+			$max = count($this->_users);
+			$n = (int)$this->in("Number of users to generate (max: $max)?", null, 0);
+			if ($n === 0) {
+				return;
+			}
+			if ($n > $max) {
+				$n = $max;
+			}
+			$users = array_rand($this->_users, $n);
+			$i = 0;
+			foreach ($users as $user) {
+				$name = $this->_users[$user];
+				$data = [
+					'User' => [
+						'username' => $name,
+						'password' => 'test',
+						'password_confirm' => 'test',
+						'user_email' => "$name@example.com"
+					]
+				];
+				$this->User->register($data, true);
+				$this->_progress($i++, $n);
+			}
+
+			$this->out();
+			$this->out("Generated $i users.");
+
+		}
+
+		protected function _progress($i, $off) {
+			if ($i < 1) {
+				return;
+			}
+			$this->out('.', 0);
+			if ($i > 1 && !($i % 50)) {
+				$percent = (int)floor($i/$off * 100);
+				$this->out(sprintf(' %3s%%', $percent), 1);
+			}
 		}
 
 		protected function _randomCategory() {
@@ -77,7 +129,8 @@
 
 		protected function _randomUser() {
 			if ($this->_Users === null) {
-				$this->_Users = $this->User->find('all', ['recursive' => 0, 'conditions' => ['activate_code' => 0]]);
+				$this->_Users = $this->User->find('all',
+					['recursive' => -1, 'conditions' => ['activate_code' => 0]]);
 			}
 			$id = array_rand($this->_Users);
 			return $this->_Users[$id]['User'];
