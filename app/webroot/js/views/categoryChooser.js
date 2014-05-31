64daadc9fe3de1b2d02eef88c16daffb75a2f53c
Schlaefer <openmail+sourcecode@siezi.com>
2014-05-31 08:02:37 +0200

    code formatting

--- app/webroot/js/views/categoryChooser.js
+++ app/webroot/js/views/categoryChooser.js
@@ -3,25 +3,24 @@ define([
   'underscore',
   'backbone'
 ], function($, _, Backbone) {
-
-  "use strict";
+  'use strict';
 
   return Backbone.View.extend({
 
     initialize: function() {
       var _$categoryChooser = $('#btn-category-chooser'),
-          _$window = $(window),
-          _x = _$categoryChooser.offset().left + _$categoryChooser.width() -
-              _$window.scrollLeft() - 410,
-          _y = _$categoryChooser.offset().top - _$window.scrollTop() +
-              _$categoryChooser.height();
+        $window = $(window),
+        x = _$categoryChooser.offset().left + _$categoryChooser.width() -
+          $window.scrollLeft() - 410,
+        y = _$categoryChooser.offset().top - $window.scrollTop() +
+          _$categoryChooser.height();
 
       this.$el.dialog({
         autoOpen: false,
-        show: {effect: "scale", duration: 200},
-        hide: {effect: "fade", duration: 200},
+        show: {effect: 'scale', duration: 200},
+        hide: {effect: 'fade', duration: 200},
         width: 400,
-        position: [_x, _y],
+        position: [x, y],
         title: $.i18n.__('Categories'),
         resizable: false
       });
@@ -35,8 +34,5 @@ define([
       }
     }
 
-
   });
-
 });
-
