68de39b28524d274db90e69c57777dbbd324b277
Schlaefer <openmail+sourcecode@siezi.com>
2014-10-06 07:26:32 +0200

    code formatting

--- app/Model/Setting.php
+++ app/Model/Setting.php
@@ -1,136 +1,135 @@
 <?php
 
-class Setting extends AppModel {
+	class Setting extends AppModel {
 
-	public $name = 'Setting';
+		public $name = 'Setting';
 
-	public $primaryKey = 'name';
+		public $primaryKey = 'name';
 
-	public $validate = array (
-		/*
+		public $validate = array(/*
 		'name' => array (
 				'rule' => 'VALID_NOT_EMPTY',
 				'message' => 'FATAL: No variable name specified'
 			),
 		*/
-	);
+		);
 
-	protected $_optionalEmailFields = [
-		'email_contact', 'email_register', 'email_system'
-	];
-
-	/* @td getSettings vs Load why to functions? */
-
-/**
- * Reads settings from DB and returns them in a compact array
- *
- * Note that this is the stored config in the DB. It may differ from the
- * current config used by the app in Config::read('Saito.Settings'), e.g.
- * when modified with a load-preset.
- *
- * @throws UnexpectedValueException
- * @return array Settings
- */
-	public function getSettings() {
-		$settings = $this->find('all');
-		if (empty($settings)) {
-			throw new UnexpectedValueException('No settings found in settings table.');
-		}
-		$settings = $this->_compactKeyValue($settings);
-
-		// extract ranks
-		$ranks = $this->_pipeSplitter($settings['userranks_ranks']);
-		ksort($ranks);
-		$settings['userranks_ranks'] = $ranks;
-
-		// edit_delay is normed to seconds
-		$this->_normToSeconds($settings, 'edit_delay');
-		$this->_fillOptionalEmailAddresses($settings);
-
-		return $settings;
-	}
-
-/**
- * Loads settings from storage into Configuration `Saito.Settings`
- *
- * ### Options
- *
- * - `force` Force reread of from storage
- *
- * @param array $preset allows to overwrite loaded values
- * @param array
- * @return array Settings
- */
-	public function load($preset = [], $options = []) {
-		Stopwatch::start('Settings->getSettings()');
-
-		$options += [
-			'force' => false
+		protected $_optionalEmailFields = [
+			'email_contact', 'email_register', 'email_system'
 		];
 
-		if ($options['force']) {
-			$settings = $this->_load();
-		} else {
-			$settings = Cache::read('Saito.appSettings');
+		/* @td getSettings vs Load why to functions? */
+
+		/**
+		 * Reads settings from DB and returns them in a compact array
+		 *
+		 * Note that this is the stored config in the DB. It may differ from the
+		 * current config used by the app in Config::read('Saito.Settings'), e.g.
+		 * when modified with a load-preset.
+		 *
+		 * @throws UnexpectedValueException
+		 * @return array Settings
+		 */
+		public function getSettings() {
+			$settings = $this->find('all');
 			if (empty($settings)) {
+				throw new UnexpectedValueException('No settings found in settings table.');
+			}
+			$settings = $this->_compactKeyValue($settings);
+
+			// extract ranks
+			$ranks = $this->_pipeSplitter($settings['userranks_ranks']);
+			ksort($ranks);
+			$settings['userranks_ranks'] = $ranks;
+
+			// edit_delay is normed to seconds
+			$this->_normToSeconds($settings, 'edit_delay');
+			$this->_fillOptionalEmailAddresses($settings);
+
+			return $settings;
+		}
+
+		/**
+		 * Loads settings from storage into Configuration `Saito.Settings`
+		 *
+		 * ### Options
+		 *
+		 * - `force` Force reread of from storage
+		 *
+		 * @param array $preset allows to overwrite loaded values
+		 * @param array
+		 * @return array Settings
+		 */
+		public function load($preset = [], $options = []) {
+			Stopwatch::start('Settings->getSettings()');
+
+			$options += [
+				'force' => false
+			];
+
+			if ($options['force']) {
 				$settings = $this->_load();
+			} else {
+				$settings = Cache::read('Saito.appSettings');
+				if (empty($settings)) {
+					$settings = $this->_load();
+				}
+			}
+
+			if ($preset) {
+				$settings = array_merge($settings, $preset);
+			}
+
+			Configure::write('Saito.Settings', $settings);
+			Stopwatch::end('Settings->getSettings()');
+		}
+
+		protected function _load() {
+			$settings = $this->getSettings();
+			Cache::write('Saito.appSettings', $settings);
+			return $settings;
+		}
+
+		public function afterSave($created, $options = array()) {
+			parent::afterSave($created, $options);
+			$this->load(null, ['force' => true]);
+		}
+
+		public function clearCache() {
+			Cache::delete('Saito.appSettings');
+		}
+
+		/**
+		 * Returns a key-value array
+		 *
+		 * Fast version of Set::combine($results, '{n}.Setting.name', '{n}.Setting.value');
+		 *
+		 * @param array $results
+		 * @return array
+		 */
+		protected function _compactKeyValue($results) {
+			$settings = array();
+			foreach ($results as $result) {
+				$settings[$result[$this->alias]['name']] = $result[$this->alias]['value'];
+			}
+			return $settings;
+		}
+
+		protected function _normToSeconds(&$settings, $field) {
+			$settings[$field] = (int)$settings[$field] * 60;
+		}
+
+		/**
+		 * Defaults optional email addresses to main address
+		 *
+		 * @param $settings
+		 */
+		protected function _fillOptionalEmailAddresses(&$settings) {
+			foreach ($this->_optionalEmailFields as $field) {
+				if (empty($settings[$field])) {
+					$settings[$field] = $settings['forum_email'];
+				}
 			}
 		}
 
-		if ($preset) {
-			$settings = array_merge($settings, $preset);
-		}
-
-		Configure::write('Saito.Settings', $settings);
-		Stopwatch::end('Settings->getSettings()');
 	}
-
-	protected function _load() {
-		$settings = $this->getSettings();
-		Cache::write('Saito.appSettings', $settings);
-		return $settings;
-	}
-
-	public function afterSave($created, $options = array()) {
-		parent::afterSave($created, $options);
-		$this->load(null, ['force' => true]);
-	}
-
-	public function clearCache() {
-		Cache::delete('Saito.appSettings');
-	}
-
-/**
- * Returns a key-value array
- *
- * Fast version of Set::combine($results, '{n}.Setting.name', '{n}.Setting.value');
- *
- * @param array $results
- * @return array
- */
-	protected function _compactKeyValue($results) {
-		$settings = array();
-		foreach ($results as $result) {
-			$settings[$result[$this->alias]['name']] = $result[$this->alias]['value'];
-		}
-		return $settings;
-	}
-
-	protected function _normToSeconds(&$settings, $field) {
-		$settings[$field] = (int)$settings[$field] * 60;
-	}
-
-	/**
-	 * Defaults optional email addresses to main address
-	 *
-	 * @param $settings
-	 */
-	protected function _fillOptionalEmailAddresses(&$settings) {
-		foreach ($this->_optionalEmailFields as $field) {
-			if (empty($settings[$field])) {
-				$settings[$field] = $settings['forum_email'];
-			}
-		}
-	}
-
-}
