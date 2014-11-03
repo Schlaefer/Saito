d188953c4dfaa5e06360be3fd9facab3a570b909
Schlaefer <openmail+sourcecode@siezi.com>
2014-11-03 19:55:45 +0100

    fixes postgresql support

--- app/Test/Fixture/UserReadFixture.php
+++ app/Test/Fixture/UserReadFixture.php
@@ -25,8 +25,8 @@ class UserReadFixture extends CakeTestFixture {
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
-			'user_id' => array('column' => 'user_id', 'unique' => 0),
-			'entry_id' => array('column' => 'entry_id', 'unique' => 0)
+			'userread_user_id' => array('column' => 'user_id', 'unique' => 0),
+			'userread_entry_id' => array('column' => 'entry_id', 'unique' => 0)
 		),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
