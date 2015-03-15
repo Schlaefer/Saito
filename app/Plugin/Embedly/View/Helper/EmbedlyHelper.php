0e781b053d06743e9e480855f565380222c58493
Schlaefer <openmail+sourcecode@siezi.com>
2015-03-15 08:41:16 +0100

    code formatting

--- app/Plugin/Embedly/View/Helper/EmbedlyHelper.php
+++ app/Plugin/Embedly/View/Helper/EmbedlyHelper.php
@@ -21,43 +21,38 @@ class EmbedlyHelper extends AppHelper {
 
 		$out = false;
 
-		$api = new Embedly\Embedly(
-			array(
-				'user_agent' => 'Mozilla/5.0 (compatible; cakephp/1.0)',
-				'key' => self::$_apiKey,
-			)
-		);
-
-		$request = ['urls' => [$string]];
+		$properties = [
+			'user_agent' => 'Mozilla/5.0 (compatible; cakephp/1.0)',
+			'key' => self::$_apiKey,
+		];
+		$api = new Embedly\Embedly($properties);
 
 		try {
+			$request = ['urls' => [$string]];
 			$obj = current($api->oembed($request));
 		} catch (Exception $e) {
 			return 'Embedding failded: ' . $e->getMessage();
 		}
 
-		if (isset($obj->html)):
+		if (isset($obj->html)) {
 			// use the html code from embedly if possible
 			$out = $obj->html;
-		elseif (isset($obj->title) && isset($obj->url)):
+		} elseif (isset($obj->title) && isset($obj->url)) {
 			// else just link to target
 			$title = '';
 			$escape = true;
-			if (isset($obj->thumbnail_url)) :
+			if (isset($obj->thumbnail_url)) {
 				// use thumbnail for link if available
 				$title .= $this->Html->image(
-					$obj->thumbnail_url, array(
-					'class' => 'embedly-image'
-				)
+					$obj->thumbnail_url,
+					['class' => 'embedly-image']
 				);
 				$title .= $this->Html->tag('br');
 				$escape = false;
-			endif;
+			}
 			$title .= $obj->title;
-			$out = $this->Html->link(
-				$title, $obj->url, array('escape' => $escape)
-			);
-		endif;
+			$out = $this->Html->link($title, $obj->url, ['escape' => $escape]);
+		}
 
 		return $out;
 	}
