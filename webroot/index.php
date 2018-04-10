00776995eaf11f3ad8ea83c39aafdfee65562df5
Schlaefer <openmail+sourcecode@siezi.com>
2018-04-10 11:39:41 +0200

    updated webroot/index from Cake3 skeleton

--- webroot/index.php
+++ webroot/index.php
@@ -2,19 +2,23 @@
 /**
  * The Front Controller for handling every request
  *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
  * @since         0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @license       MIT License (https://opensource.org/licenses/mit-license.php)
  */
-// for built-in server
+
+// Check platform requirements
+require dirname(__DIR__) . '/config/requirements.php';
+
+// For built-in server
 if (php_sapi_name() === 'cli-server') {
     $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
 
@@ -24,14 +28,13 @@ if (php_sapi_name() === 'cli-server') {
         return false;
     }
 }
-require dirname(__DIR__) . '/config/bootstrap.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-use Cake\Network\Request;
-use Cake\Network\Response;
-use Cake\Routing\DispatcherFactory;
+use App\Application;
+use Cake\Http\Server;
 
-$dispatcher = DispatcherFactory::create();
-$dispatcher->dispatch(
-    Request::createFromGlobals(),
-    new Response()
-);
+// Bind your application to the server.
+$server = new Server(new Application(dirname(__DIR__) . '/config'));
+
+// Run the request/response through the application and emit the response.
+$server->emit($server->run());
