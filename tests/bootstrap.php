8d967b6ca2f882c1882eda51ea5904f07595a816
Schlaefer <openmail+sourcecode@siezi.com>
2018-04-10 14:27:12 +0200

    set debug mode for tests, required in Cake3

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,3 +10,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 require dirname(__DIR__) . '/config/bootstrap.php';
 
 $_SERVER['PHP_SELF'] = '/';
+
+// otherwise Security mock fails with debug info
+use Cake\Core\Configure;
+Configure::write('debug', true);
