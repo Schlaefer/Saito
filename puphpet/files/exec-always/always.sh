9af08c8657e86c63ed2348491b8e00c9fabbc053
Schlaefer <openmail+sourcecode@siezi.com>
2018-06-19 14:10:25 +0200

    remove puphpet

--- puphpet/files/exec-always/always.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# sets rights for session creation in CakePHP CLI tests
-sudo chmod -R  ugo+rwx  /var/lib/php;
-
-# Cake alias
-alias cake='/var/www/app/Console/cake'
--- puphpet/files/exec-once/99-finish.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# init vagrant database config
-cp /vagrant/app/Config/database.php.vagrant /vagrant/app/Config/database.php;
