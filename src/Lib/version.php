2e7abe43acc94bd40e0ef491a45bacfee5fa2d93
Schlaefer <openmail+sourcecode@siezi.com>
2019-11-08 21:22:21 +0100

    Bumps version string to 5.5.0

--- src/Lib/version.php
+++ src/Lib/version.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 $config = [
     'Saito' =>
         [
-            'v' => '5.4.1',
+            'v' => '5.5.0',
             'saitoHomepage' => 'https://saito.siezi.com/'
         ]
 ];
